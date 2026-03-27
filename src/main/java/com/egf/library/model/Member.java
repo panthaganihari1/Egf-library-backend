28f53938d1b29ebbf13b251a1dfbe7903bbe7e8a
Gani Hari <panthaganihari1@gmail.com>
2026-03-28 00:25:27 +0530

    Initial backend commit

---
+++ src/main/java/com/egf/library/model/Member.java
@@ -0,0 +1,34 @@
+package com.egf.library.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    private String email;
+
+    private String address;
+
+    @Column(updatable = false)
+    private LocalDateTime joinedAt = LocalDateTime.now();
+
+    private boolean active = true;
+}
